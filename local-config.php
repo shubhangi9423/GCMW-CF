b13d2df8a06e909454d814b00aeac75ada56df01
Shubangi Wani <ShubangiW@CCMT.local>
2017-11-10 10:20:18 +0530

    created enivorment

---
+++ local-config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'ccf_wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
